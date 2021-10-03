3d1b7226ecf0e1db8645dae3723cfd1ae6fe16cd
AshutoshSajan <ashutoshsajan1213@gmail.com>
2021-10-03 12:10:03 +0530

    complete import, print, constansts

--- constants/main.go
+++ constants/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+const Pi = 3.14
+const (
+	StatusOk = 200
+	StatusCreate = 201
+	StatusAccepted = 202
+)
+
 
 func main() {
-	fmt.Println("Constants in go")
+	// fmt.Println("Constants in go")
+	// fmt.Println(StatusOk)
+
+	// name := "sajan"
+	// aka := fmt.Sprintf("Number %d", 6)
+
+	// fmt.Println(aka)
+
+	// fmt.Printf("%s is also known as %s", name, aka)
+
+	fmt.Println(math.Pi)
 }
